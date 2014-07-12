2478a82a26dc34b300dd9f2266afaf3a2a872e37
Nicolas Brechet <nicolasbrechet@me.com>
2014-07-12 20:01:28 +0200

    Testing config

--- site/manifests/profiles/multiphp.pp
+++ site/manifests/profiles/multiphp.pp
@@ -4,31 +4,30 @@ class site::profiles::multiphp {
     
   }
   
-  apache::mod { 'mod_proxy': }
-  apache::mod { 'mod_proxy_fcgi': }
+  apache::mod { 'proxy': }
+  apache::mod { 'proxy_fcgi': }
   
   apache::vhost { 'truc1.com':
     port            => '80',
-    docroot         => '/var/www/html',
-    custom_fragment => 'ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9001/var/www/html/$1'
+    docroot         => '/var/www/html/truc1',
+    custom_fragment => 'ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9001/var/www/html/truc1/$1'
   }
   
   apache::vhost { 'truc2.com':
     port            => '80',
-    docroot         => '/var/www/html',
-    custom_fragment => 'ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9002/var/www/html/$1'
+    docroot         => '/var/www/html/truc2',
+    custom_fragment => 'ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9002/var/www/html/truc2/$1'
   }
   
   apache::vhost { 'truc3.com':
     port            => '80',
-    docroot         => '/var/www/html',
-    custom_fragment => 'ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9003/var/www/html/$1'
+    docroot         => '/var/www/html/truc3',
+    custom_fragment => 'ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9003/var/www/html/truc3/$1'
   }
   
   include php
-  class { 'php::fpm': 
-    ensure => installed,
-  }
+  include php::fpm
+  include php::params
   
   php::fpm::pool { 'truc1':
     listen => '127.0.0.1:9001',
@@ -42,13 +41,15 @@ class site::profiles::multiphp {
     listen => '127.0.0.1:9003',
   }
   
-  create_resources('php::fpm::config',  hiera_hash('php_fpm_config', []))
+  #create_resources('php::fpm::pool',  hiera_hash('php_fpm_pool', {}))
+  create_resources('php::fpm::config',  hiera_hash('php_fpm_config', {}))
   
   # Install extensions
   Php::Extension <| |>
     # Configure extensions
     -> Php::Config <| |>
     # Reload webserver
+    ~> Service["php5-fpm"]
     ~> Service["apache2"]
   
 }
