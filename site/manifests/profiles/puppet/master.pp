3935bc8542402e731f9d638ab85d6c10a6a46641
Nicolas Brechet <nicolasbrechet@me.com>
2014-08-14 12:04:42 +0200

    ssh config shoudl be in base profile

--- site/manifests/profiles/puppet/master.pp
+++ site/manifests/profiles/puppet/master.pp
@@ -6,13 +6,6 @@ class site::profiles::puppet::master {
     require => Anchor['::site::profiles::puppet::master']
   }
   
-  class { 'ssh::server':
-    options => {
-      'PasswordAuthentication'  => 'no',
-      'PermitRootLogin'         => 'yes',
-    },
-  }
-  
   class { '::puppet::server':
     modulepath         => ['$confdir/environments/$environment/modules', '$confdir/environments/$environment/'],
     manifest           => '$confdir/environments/$environment/site/manifests/site.pp',
