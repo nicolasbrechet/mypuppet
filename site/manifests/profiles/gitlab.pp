7d690bd2ea2832859c1929cc0f20a86a203e25ee
Nicolas Brechet <nicolasbrechet@me.com>
2014-08-14 14:05:46 +0200

    specified HTTP and SSL port

--- site/manifests/profiles/gitlab.pp
+++ site/manifests/profiles/gitlab.pp
@@ -16,6 +16,8 @@ class site::profiles::gitlab {
 	}
 
 	class { '::gitlab':
+    gitlab_http_port        => '1080',
+    gitlab_ssl_port         => '443',
 		git_email								=> "gitlab@nicolasbrechet.com",
 	  gitlab_domain     			=> 'gitlab.nicolasbrechet.com',
 		gitlab_branch						=> '7-1-stable',
