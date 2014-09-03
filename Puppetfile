7a0866355ee19b874a20d8c5983caea6401a1577
Nicolas Brechet <nicolasbrechet@me.com>
2014-09-03 15:58:31 +0200

    Updated Puppetfile & gitlab conf

--- Puppetfile
+++ Puppetfile
@@ -1,34 +1,17 @@
 forge 'forge.puppetlabs.com'
 
-# # Common modules
-##################
-#
-# Git : Check version requirement
-#
 mod 'puppetlabs/git', '0.1.0'
 
-#
-# Sudo
-#
 mod 'sudo', :git => 'https://github.com/saz/puppet-sudo.git', :ref => 'v3.0.1'
 
-
-
-#
-# Puppet Master modules
-#
 mod 'puppetlabs/passenger',   '0.2.0'
 mod 'puppetlabs/postgresql',  '3.3.3'
 mod 'puppetlabs/ruby',        '0.1.0'
 mod 'puppetlabs/inifile',     '1.1.0'
 mod 'jfryman/nginx',          '0.0.9'
-mod 'puppetdb', :git => "https://github.com/puppetlabs/puppetlabs-puppetdb.git"
+mod 'puppetdb', :git => "https://github.com/puppetlabs/puppetlabs-puppetdb.git", :ref => 'cbcf042f5418ae1d8943588034f372817b3955cf'
 #mod 'puppet',   :git => "https://github.com/gloppasglop/puppet-puppet.git"
 mod 'puppet',   :git => "https://github.com/nicolasbrechet/puppet-puppet.git", :ref => 'veltigroup'
-
-#
-# Ntp
-#
 
 mod 'puppetlabs/ntp',         '3.1.1'
 mod 'puppetlabs/apt',         '1.6.0'
@@ -37,19 +20,14 @@ mod 'puppetlabs/firewall',    '1.1.2'
 mod 'puppetlabs/stdlib',      '4.2.2'
 mod 'puppetlabs/vcsrepo',     '1.0.1'
 mod 'richardc/datacat',       '0.6.0'
-mod 'apache' ,        :git => "https://github.com/puppetlabs/puppetlabs-apache"
+#mod 'apache' ,        :git => "https://github.com/puppetlabs/puppetlabs-apache"
 mod 'puppetlabs_apt', :git => 'https://github.com/puppetlabs-operations/puppet-puppetlabs_apt.git', :ref => '0.0.3'
-
-#
-# Puppetboard
-#
 
 mod 'puppetboard', :git => 'https://github.com/nibalizer/puppet-module-puppetboard.git', :ref => '2.5.0'
 mod 'stankevich/python', '1.7.5'
 
-#
-# PHP
-#
+mod 'puppetlabs/apache', '1.1.1'
+
 mod 'php', :git => 'https://github.com/jippi/puppet-php'
 
 #
--- site/manifests/profiles/gitlab.pp
+++ site/manifests/profiles/gitlab.pp
@@ -1,52 +1,31 @@
 class site::profiles::gitlab {
-	$gitlab_dbname	= "nbt_gitlab_db"
-	$gitlab_dbuser	= "nbt_gitlab_user"
-	$gitlab_dbpwd 	= "XYHLYFstit36}]z3iYVw"
-
-  #exec { 'initial update':
-  #  command   => '/usr/bin/apt-get update',
-  #}
-  #
-  #Exec['initial update'] -> Package <| |>
-
-  class {'nginx':
-    confd_purge => true,
+  $gitlab_dbname	= hiera('gitlab::gitlab_dbname')
+  $gitlab_dbuser	= hiera('gitlab::gitlab_dbuser')
+  $gitlab_dbpwd 	= hiera('gitlab::gitlab_dbpwd')
+  
+  class { '::gitlab_requirements':
+    gitlab_dbname   => $gitlab_dbname,
+    gitlab_dbuser   => $gitlab_dbuser,
+    gitlab_dbpwd    => $gitlab_dbpwd,
+	}
+	  
+  class { '::gitlab':
+    git_email           => hiera('gitlab::git_email'),
+    gitlab_domain       => hiera('gitlab::gitlab_domain'),
+    gitlab_branch       => hiera('gitlab::gitlab_branch'),
+    gitlabshell_branch  => hiera('gitlab::gitlabshell_branch'),
+    gitlab_dbtype       => hiera('gitlab::gitlab_dbtype'),
+    gitlab_dbname       => $gitlab_dbname,
+    gitlab_dbuser       => $gitlab_dbuser,
+    gitlab_dbpwd        => $gitlab_dbpwd
   }
   
-	class { 'gitlab_requirements':
-	  gitlab_dbname   => $gitlab_dbname,
-	  gitlab_dbuser   => $gitlab_dbuser,
-	  gitlab_dbpwd    => $gitlab_dbpwd,
-	}
-
-	class { '::gitlab':
-    gitlab_unicorn_port     => '18080',
-		git_email								=> "gitlab@nicolasbrechet.com",
-	  gitlab_domain     			=> 'gitlab.nicolasbrechet.com',
-		gitlab_branch						=> '7-1-stable',
-		gitlabshell_branch			=> 'v1.9.6',
-    gitlab_manage_nginx     => 'false',
-	  gitlab_dbtype     			=> 'mysql',
-	  gitlab_dbname     			=> $gitlab_dbname,
-	  gitlab_dbuser     			=> $gitlab_dbuser,
-	  gitlab_dbpwd      			=> $gitlab_dbpwd,			                  			
-		gitlab_ssl      				=> true,
-		gitlab_ssl_cert   			=> "/etc/ssl/certs/ssl-cert-snakeoil.pem",
-		gitlab_ssl_key    			=> "/etc/ssl/private/ssl-cert-snakeoil.key",
-		gitlab_ssl_self_signed	=> true,
-		ldap_enabled    				=> false,
-	}
-  
-  
-  apache::vhost { 'gitlab.nicolasbrechet.com':
-    docroot               => '/home/git/gitlab/public',
-    ssl                   => true,
-    ssl_protocol          => 'all -SSLv2',
-    ssl_honorcipherorder  => 'on',
-    ssl_cipher            => '"ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS"',
-    
-    
+  package{'bundler':
+    ensure => present,
+    provider => 'gem',    
   }
   
-	Class['gitlab_requirements'] -> Class['gitlab']
+  Package['bundler'] 
+  -> Class['::gitlab_requirements'] 
+  -> Class['::gitlab']
 }
