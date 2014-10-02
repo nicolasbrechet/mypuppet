6fea4e033224abcdfe9ccae5895a6cc358ab67ba
Nicolas Brechet <nicolasbrechet@me.com>
2014-10-02 17:10:33 +0200

    MySQL

--- site/manifests/profiles/icinga.pp
+++ site/manifests/profiles/icinga.pp
@@ -5,6 +5,31 @@ class site::profiles::icinga {
   }
   
   include icinga
-  include '::mysql::server'
+  class {'::mysql::server':
+  } ->
+  mysql_database { 'icinga':
+    ensure  => 'present',
+    charset => 'utf8',
+    collate => 'utf8_swedish_ci',
+  } ->
+  mysql_database { 'icinga_web':
+    ensure  => 'present',
+    charset => 'utf8',
+    collate => 'utf8_swedish_ci',
+  } ->  
+  mysql_user { 'icinga@localhost':
+    ensure                   => 'present',
+    max_connections_per_hour => '0',
+    max_queries_per_hour     => '0',
+    max_updates_per_hour     => '0',
+    max_user_connections     => '0',
+  } ->  
+  mysql_grant { 'icinga@localhost/*.*':
+    ensure     => 'present',
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table      => '*.*',
+    user       => 'icinga@localhost',
+  }
 
 }
