f90c0bf12733f821ad193114bb86b4a23a3cf277
Nicolas Brechet <nicolasbrechet@me.com>
2014-10-08 11:27:27 +0200

    No configuration for monitoring_server

--- site/manifests/profiles/monitoring_server.pp
+++ site/manifests/profiles/monitoring_server.pp
@@ -1,36 +1,3 @@
 class site::profiles::monitoring_server {
   
-  class {'::mysql::server':
-  } ->
-  mysql::db { 'icinga DB':
-    dbname    => hiera('icinga::params::ido_db_name'),
-    user      => hiera('icinga::params::ido_db_user'),
-    password  => hiera('icinga::params::ido_db_pass'),
-    host      => hiera('icinga::params::ido_db_host'),
-  } ->
-  mysql::db { 'icinga web DB':
-    dbname    => hiera('icinga::params::web_db_name'),
-    user      => hiera('icinga::params::web_db_user'),
-    password  => hiera('icinga::params::web_db_pass'),
-    host      => hiera('icinga::params::web_db_host')
-  }
-  
-  include icinga
-  include pnp4nagios
-  include nrpe
-  include nrpe::monitoring
-  
-  group {'nrpe':
-    ensure => present,
-  }
-  
-  user {'nrpe':
-    ensure => present,
-  }
-  
-  Group['nrpe'] 
-  -> User['nrpe'] 
-  -> Class['nrpe'] 
-  -> Class['nrpe::monitoring']
-  
 }
