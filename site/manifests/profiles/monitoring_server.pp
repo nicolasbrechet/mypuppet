26345cb2246a1f6c0df045d88c4676aceec85aa8
Nicolas Brechet <nicolasbrechet@me.com>
2014-10-08 12:11:45 +0200

    Added icinga-web

--- site/manifests/profiles/monitoring_server.pp
+++ site/manifests/profiles/monitoring_server.pp
@@ -1,11 +1,13 @@
 class site::profiles::monitoring_server {
   # add the Ubuntu PPA for Icinga
   apt::ppa { 'ppa:formorer/icinga': }
+  apt::ppa { 'ppa:formorer/icinga-web': }
+  
   apt_key { 'ppa-formorer-key':
       ensure => 'present',
       id     => '36862847'
   } ->  
-  package{'icinga':
+  package{['icinga', 'icinga-doc', 'icinga-idoutils', 'icinga-web']:
     ensure => installed,
   }
 }
