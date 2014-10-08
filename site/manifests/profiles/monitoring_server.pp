a65004a20180199e672c10fb8a8386f612ceae81
Nicolas Brechet <nicolasbrechet@me.com>
2014-10-08 15:06:16 +0200

    Removed ordering

--- site/manifests/profiles/monitoring_server.pp
+++ site/manifests/profiles/monitoring_server.pp
@@ -6,7 +6,7 @@ class site::profiles::monitoring_server {
   apt_key { 'ppa-formorer-key':
       ensure => 'present',
       id     => '36862847'
-  } -> 
+  } 
   class { '::icinga::server':
     icinga_configure_webserver  => true,
     icinga_webserver            => 'apache2', #default
@@ -15,14 +15,17 @@ class site::profiles::monitoring_server {
     
     # => Generates /etc/icinga/monitoring.nicolasbrechet.com-apache2.conf.example 
   } ->
-  class { 'apache':
-    default_vhost => false,
-  } ->
   file { "My apache vhost config":
     target  => '/etc/icinga/monitoring.nicolasbrechet.com-apache2.conf.example',
     ensure  => 'link',
     path    => '/etc/apache2/sites-enabled/25-monitoring.nicolasbrechet.com-apache2.conf',
   }
+  
+  class { 'apache':
+    default_vhost => false,
+  }
+  
+
 
   
 }
