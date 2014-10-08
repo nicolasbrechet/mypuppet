fd01c02ff8435d88d51157e875b45cd81367b47f
Nicolas Brechet <nicolasbrechet@me.com>
2014-10-08 12:05:37 +0200

    Added formorer icinga ppa

--- site/manifests/profiles/monitoring_server.pp
+++ site/manifests/profiles/monitoring_server.pp
@@ -1,3 +1,11 @@
 class site::profiles::monitoring_server {
-  
+  # add the Ubuntu PPA for Icinga
+  apt::ppa { 'ppa:formorer/icinga': }
+  apt_key { 'ppa-formorer-key':
+      ensure => 'present',
+      id     => '36862847'
+  } ->  
+  package{'icinga':
+    ensure => installed,
+  }
 }
