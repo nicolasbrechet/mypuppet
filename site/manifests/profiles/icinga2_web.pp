9b076066c038205ceb6fafdb55aa261f0b7d49fa
Nicolas Brechet <nicolasbrechet@me.com>
2014-10-10 12:16:12 +0200

    Ordering + corrected source of sql import

--- site/manifests/profiles/icinga2_web.pp
+++ site/manifests/profiles/icinga2_web.pp
@@ -12,36 +12,34 @@ class site::profiles::icinga2_web {
     provider => git,
     source   => 'git@github.com:Icinga/icingaweb2.git',
     revision => 'c67e7c3ef0cc468b666a75d2bc4c727c3faa5c64',
-  } ->  
+  } 
+  
   concat { "/opt/sqlimport.sql":
     owner   => 'root',
     group   => 'root',
     mode    => '0644'
-  } ->
+  } 
+  
+  concat::fragment { 'accounts':
+    target  => "/opt/sqlimport.sql",
+    source  => '/opt/icingaweb2/etc/schema/accounts.mysql.sql',
+    order   => '01'
+  } 
+  
+  concat::fragment { 'preferences':
+    target  => "/opt/sqlimport.sql",
+    source  => '/opt/icingaweb2/etc/schema/preferences.mysql.sql',
+    order   => '02'
+  } 
+  
   mysql::db { $icingaweb2_db_name:
     user     => $icingaweb2_user,
     password => $icingaweb2_password,
     sql      => '/opt/sqlimport.sql',
-    #require  => Concat["/opt/sqlimport.sql"] #, concat::fragment['accounts'], concat::fragment['preferences']],
   }
   
-  
-  
-  
-  
-  concat::fragment { 'accounts':
-    target  => "/opt/sqlimport.sql",
-    content => '/opt/icingaweb2/etc/schema/accounts.mysql.sql',
-    order   => '01'
-  } 
-  concat::fragment { 'preferences':
-    target  => "/opt/sqlimport.sql",
-    content => '/opt/icingaweb2/etc/schema/preferences.mysql.sql',
-    order   => '02'
-  } 
-
-  
-
-  
+  Vcsrepo["/opt/icingaweb2"] <| |> 
+    -> Concat["/opt/sqlimport.sql"] <| |> 
+    -> Mysql::Db[$icingaweb2_db_name] <| |> 
   
 }
