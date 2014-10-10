27cf76208eaca56bb31cc060c41ab65a1b4a91cd
Nicolas Brechet <nicolasbrechet@me.com>
2014-10-10 11:18:28 +0200

    Concat of 2 sql file for initial import

--- site/manifests/profiles/icinga2_web.pp
+++ site/manifests/profiles/icinga2_web.pp
@@ -4,25 +4,39 @@ class site::profiles::icinga2_web {
   $icingaweb2_user = 'icinga2web'
   $icingaweb2_password = 'password'
   
+  vcsrepo { "/opt/icingaweb2":
+    ensure   => present,
+    provider => git,
+    source   => 'git@github.com:Icinga/icingaweb2.git',
+    revision => 'c67e7c3ef0cc468b666a75d2bc4c727c3faa5c64',
+  } ->
+  concat { "/opt/sqlimport.sql":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644'
+  } 
+  
+  concat::fragment { 'accounts':
+    target  => "/opt/sqlimport.sql",
+    content => '/opt/icingaweb2/etc/schema/accounts.mysql.sql',
+    order   => '01'
+  }
+  
+  concat::fragment { 'preferences':
+    target  => "/opt/sqlimport.sql",
+    content => '/opt/icingaweb2/etc/schema/preferences.mysql.sql',
+    order   => '02'
+  }
+  
   # MySQL DB Setup
   include '::mysql::server'
   
   mysql::db { $icingaweb2_db_name:
     user     => $icingaweb2_user,
     password => $icingaweb2_password,
-    sql      => '/opt/icingaweb2/etc/schema/accounts.mysql.sql',
+    sql      => '/opt/sqlimport.sql',
     require  => Vcsrepo["/opt/icingaweb2"],
-  } ->
-  exec {"mysql -u $icingaweb2_user --password $icingaweb2_password $icingaweb2_db_name < /opt/icingaweb2/etc/schema/preferences.mysql.sql":
-  }  
-  
-  vcsrepo { "/opt/icingaweb2":
-    ensure   => present,
-    provider => git,
-    source   => 'git@github.com:Icinga/icingaweb2.git',
-    revision => 'c67e7c3ef0cc468b666a75d2bc4c727c3faa5c64',
   }
-  
   
   
 }
