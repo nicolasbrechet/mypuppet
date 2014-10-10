d571d4e44c84c9f0f16e946387f1e86562b4491f
Nicolas Brechet <nicolasbrechet@me.com>
2014-10-10 10:51:14 +0200

    Param db name

--- site/manifests/profiles/icinga2_web.pp
+++ site/manifests/profiles/icinga2_web.pp
@@ -1,18 +1,19 @@
 class site::profiles::icinga2_web {
   
+  $icingaweb2_db_name = 'icinga2web'
   $icingaweb2_user = 'icinga2web'
   $icingaweb2_password = 'password'
   
   # MySQL DB Setup
   include '::mysql::server'
   
-  mysql::db { 'icinga2web':
+  mysql::db { $icingaweb2_db_name:
     user     => $icingaweb2_user,
     password => $icingaweb2_password,
     sql      => '/opt/icingaweb2/etc/schema/accounts.mysql.sql',
     require  => Vcsrepo["/opt/icingaweb2"],
   } ->
-  exec {"mysql -u #{$icingaweb2_user} --password #{$icingaweb2_password} icinga2web < /opt/icingaweb2/etc/schema/preferences.mysql.sql":
+  exec {"mysql -u $icingaweb2_user --password $icingaweb2_password $icingaweb2_db_name < /opt/icingaweb2/etc/schema/preferences.mysql.sql":
   }  
   
   vcsrepo { "/opt/icingaweb2":
